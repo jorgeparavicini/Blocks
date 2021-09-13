1eda4b54d77678fb7ede7aa520f5ec3e9c41b1a1
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-09-13 14:34:26 +0200

    Create precompiled headers file

---
+++ Blocks/stdafx.h
@@ -0,0 +1,10 @@
+﻿// Copyright (c) 2021, Severin Goddon & Jorge Paravicini
+// All rights reserved.
+// 
+// This source code is licensed under the MIT-style license found in LICENSE file in the root directory of this source tree.
+// 
+// Author: Jorge Paravicini
+
+#pragma once
+
+// Include all headers to be precompiled here
