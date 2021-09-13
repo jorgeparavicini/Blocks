cbae3c945d6eab1cbacd20fe26932ed133eaf0e9
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-09-13 13:28:20 +0200

    Revert "test"
    This reverts commit 9f682ee8848118cce2ce3d9d41ff69ff08795589.

--- Blocks.Engine/Hot Garbage.h
+++
@@ -1,8 +0,0 @@
-﻿// Copyright (c) 2021, Severin Goddon & Jorge Paravicini
-// All rights reserved.
-// 
-// This source code is licensed under the MIT-style license found in LICENSE file in the root directory of this source tree.
-// 
-// Author: Jorge Paravicini
-
-#pragma once
