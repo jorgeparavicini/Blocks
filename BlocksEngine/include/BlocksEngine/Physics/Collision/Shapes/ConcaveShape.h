fcc25e61f388b19d40f11d4278f27d9a027da061
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-12-12 22:32:11 +0100

    Implement Concave Shape

--- BlocksEngine/include/BlocksEngine/Physics/Collision/Shapes/ConcaveShape.h
+++ BlocksEngine/include/BlocksEngine/Physics/Collision/Shapes/ConcaveShape.h
@@ -10,6 +10,7 @@
 #pragma once
 
 #include "CollisionShape.h"
+#include "TriangleShape.h"
 #include "BlocksEngine/Core/Math/Math.h"
 
 namespace BlocksEngine
@@ -26,6 +27,12 @@ public:
     // Constructors, Destructors, Assignment & Move
     //------------------------------------------------------------------------------
 
+    TriangleCallback(const TriangleCallback&) = delete;
+    TriangleCallback& operator=(const TriangleCallback&) = delete;
+
+    TriangleCallback(TriangleCallback&&) = delete;
+    TriangleCallback& operator=(TriangleCallback&&) = delete;
+
     virtual ~TriangleCallback() = default;
 
     //------------------------------------------------------------------------------
@@ -39,10 +46,89 @@ public:
 class BlocksEngine::ConcaveShape : public CollisionShape
 {
 public:
+    //------------------------------------------------------------------------------
+    // Constructors, Destructors, Assignment & Move
+    //------------------------------------------------------------------------------
+
+    ConcaveShape(CollisionShapeName name, const Vector3<float>& scaling);
+
+    ConcaveShape(const ConcaveShape&) = delete;
+    ConcaveShape& operator=(const ConcaveShape&) = delete;
+
+    ConcaveShape(ConcaveShape&&) = delete;
+    ConcaveShape& operator=(ConcaveShape&&) = delete;
+
+    ~ConcaveShape() override = default;
+
+
+    //------------------------------------------------------------------------------
+    // Methods
+    //------------------------------------------------------------------------------
+
+    [[nodiscard]] TriangleRaycastSide GetRaycastTestType() const;
+
+    void SetRaycastTestType(TriangleRaycastSide testType);
+
+    [[nodiscard]] const Vector3<float>& GetScale() const;
+
+    void SetScale(const Vector3<float>& scale);
+
+    [[nodiscard]] bool IsConvex() const override;
+
+    [[nodiscard]] bool IsPolyhedron() const override;
+
+    virtual void ComputeOverlappingTriangles(const AABB& localAABB,
+                                             std::vector<Vector3<float>>& triangleVertices,
+                                             std::vector<Vector3<float>>& triangleVertexNormals,
+                                             std::vector<int>& shapeIds) const = 0;
+
+    [[nodiscard]] float GetVolume() const override;
+
+
 protected:
     //------------------------------------------------------------------------------
     // Fields
     //------------------------------------------------------------------------------
 
     TriangleRaycastSide raycastTestType_;
+
+    Vector3<float> scale_;
+
+    [[nodiscard]] bool TestPointInside(const Vector3<>& worldPoint, const Collider& collider) const override;
 };
+
+inline bool BlocksEngine::ConcaveShape::IsConvex() const
+{
+    return false;
+}
+
+inline bool BlocksEngine::ConcaveShape::IsPolyhedron() const
+{
+    return true;
+}
+
+inline bool BlocksEngine::ConcaveShape::TestPointInside(const Vector3<>& worldPoint, const Collider& collider) const
+{
+    return false;
+}
+
+inline BlocksEngine::TriangleRaycastSide BlocksEngine::ConcaveShape::GetRaycastTestType() const
+{
+    return raycastTestType_;
+}
+
+inline void BlocksEngine::ConcaveShape::SetRaycastTestType(const TriangleRaycastSide testType)
+{
+    raycastTestType_ = testType;
+}
+
+inline const BlocksEngine::Vector3<>& BlocksEngine::ConcaveShape::GetScale() const
+{
+    return scale_;
+}
+
+inline void BlocksEngine::ConcaveShape::SetScale(const Vector3<>& scale)
+{
+    scale_ = scale;
+    NotifyShapeChanged();
+}
--- BlocksEngine/src/Physics/Collision/Shapes/ConcaveShape.cpp
+++ BlocksEngine/src/Physics/Collision/Shapes/ConcaveShape.cpp
@@ -1,2 +1,20 @@
 ﻿#include "BlocksEngine/pch.h"
-#include "BlocksEngine/ConcaveShape.h"
+#include "BlocksEngine/Physics/Collision/Shapes/ConcaveShape.h"
+
+using namespace BlocksEngine;
+
+ConcaveShape::ConcaveShape(const CollisionShapeName name, const Vector3<>& scaling)
+    : CollisionShape{CollisionShapeType::ConcaveShape, name},
+      raycastTestType_{TriangleRaycastSide::Front},
+      scale_{scaling}
+{
+}
+
+float ConcaveShape::GetVolume() const
+{
+    Vector3<float> minBounds, maxBounds;
+    GetLocalBounds(minBounds, maxBounds);
+
+    const Vector3<float> length = maxBounds - minBounds;
+    return length.x * length.y * length.z;
+}
