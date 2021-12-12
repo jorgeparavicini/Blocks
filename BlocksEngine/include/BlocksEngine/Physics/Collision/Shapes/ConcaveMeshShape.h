bce9efdbdcf7394dd0dc10c4bda5bc7db628bc5a
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-12-12 22:46:25 +0100

    // WIP ConcaveMeshShape

--- BlocksEngine/include/BlocksEngine/Physics/Collision/Shapes/ConcaveMeshShape.h
+++ BlocksEngine/include/BlocksEngine/Physics/Collision/Shapes/ConcaveMeshShape.h
@@ -8,25 +8,103 @@
 // File: ConcaveMeshShape.h
 
 #pragma once
+#include "ConcaveShape.h"
 #include "BlocksEngine/Physics/Collision/Broadphase/DynamicAABBTree.h"
 
 namespace BlocksEngine
 {
     class ConvexTriangleAABBOverlapCallback;
+    class ConcaveMeshRaycastCallback;
     class ConcaveMeshShape;
 }
 
-class BlocksEngine::ConvexTriangleAABBOverlapCallback : public DynamicAABBTreeOverlapCallback
+class BlocksEngine::ConvexTriangleAABBOverlapCallback final : public DynamicAABBTreeOverlapCallback
 {
 public:
     //------------------------------------------------------------------------------
     // Constructors, Destructors, Assignment & Move
     //------------------------------------------------------------------------------
 
-    ConvexTriangleAABBOverlapCallback(TriangleCallb)
+    ConvexTriangleAABBOverlapCallback(TriangleCallback& triangleCallback, const ConcaveMeshShape& concaveMeshShape,
+                                      const DynamicAABBTree& dynamicAABBTree);
+
+
+    //------------------------------------------------------------------------------
+    // Methods
+    //------------------------------------------------------------------------------
+
+    void NotifyOverlappingNode(int nodeId) override;
+
+private:
+    TriangleCallback& triangleTestCallback_;
+
+    const ConcaveMeshShape& concaveMeshShape_;
+
+    const DynamicAABBTree& dynamicAABBTree_;
 };
+
+
+class BlocksEngine::ConcaveMeshRaycastCallback : public DynamicAABBTreeRaycastCallback
+{
+public:
+    //------------------------------------------------------------------------------
+    // Constructors, Destructors, Assignment & Move
+    //------------------------------------------------------------------------------
+
+    ConcaveMeshRaycastCallback(const DynamicAABBTree& dynamicAABBTree,
+                               const ConcaveMeshShape& concaveMeshShape,
+                               Collider* collider, RaycastInfo& raycastInfo,
+                               const Ray& ray, const Vector3<float>& meshScale);
+
+
+    //------------------------------------------------------------------------------
+    // Methods
+    //------------------------------------------------------------------------------
+
+    float RaycastBroadPhaseShape(int32_t nodeId, const Ray& ray) override;
+
+
+    void RaycastTriangles();
+
+    bool IsHit() const;
+
+private:
+    //------------------------------------------------------------------------------
+    // Fields
+    //------------------------------------------------------------------------------
+
+    std::vector<int> hitAABBNodes_;
+    const DynamicAABBTree& dynamicAABBTree_;
+    const ConcaveMeshShape& concaveMeshShape_;
+    Collider* collider_;
+    RaycastInfo& raycastInfo_;
+    const Ray& ray_;
+    bool isHit_;
+    const Vector3<float>& meshScale_;
+};
+
 
 class BlocksEngine::ConcaveMeshShape
 {
 public:
 };
+
+
+inline void BlocksEngine::ConvexTriangleAABBOverlapCallback::NotifyOverlappingNode(const int nodeId)
+{
+    const int32_t* data = dynamicAABBTree_.GetNodeDataInt(nodeId);
+
+    Vector3<float> trianglePoints[3];
+    concaveMeshShape_.GetTriangleVertices(data[0], data[1], trianglePoints);
+
+    Vector3 verticesNormals[3];
+    concaveMeshShape_.GetTriangleVerticesNormals(data[0], data[1], verticesNormals);
+
+    triangleTestCallback_.TestTriangle(trianglePoints, verticesNormals,
+                                       concaveMeshShape_.ComputeTriangleShapeId(data[0], data[1]))
+}
+
+inline bool BlocksEngine::ConcaveMeshRaycastCallback::IsHit() const
+{
+    return isHit_;
+}
--- BlocksEngine/src/Physics/Collision/Shapes/ConcaveMeshShape.cpp
+++ BlocksEngine/src/Physics/Collision/Shapes/ConcaveMeshShape.cpp
@@ -1,2 +1,28 @@
 ﻿#include "BlocksEngine/pch.h"
 #include "BlocksEngine/Physics/Collision/Shapes/ConcaveMeshShape.h"
+
+using namespace BlocksEngine;
+
+ConvexTriangleAABBOverlapCallback::ConvexTriangleAABBOverlapCallback(TriangleCallback& triangleCallback,
+                                                                     const ConcaveMeshShape& concaveMeshShape,
+                                                                     const DynamicAABBTree& dynamicAABBTree)
+    : triangleTestCallback_{triangleCallback},
+      concaveMeshShape_{concaveMeshShape},
+      dynamicAABBTree_{dynamicAABBTree}
+{
+}
+
+ConcaveMeshRaycastCallback::ConcaveMeshRaycastCallback(const DynamicAABBTree& dynamicAABBTree,
+                                                       const ConcaveMeshShape& concaveMeshShape, Collider* collider,
+                                                       RaycastInfo& raycastInfo, const Ray& ray,
+                                                       const Vector3<>& meshScale)
+    : hitAABBNodes_{},
+      dynamicAABBTree_{dynamicAABBTree},
+      concaveMeshShape_{concaveMeshShape},
+      collider_{collider},
+      raycastInfo_{raycastInfo},
+      ray_{ray},
+      isHit_{false},
+      meshScale_{meshScale}
+{
+}
