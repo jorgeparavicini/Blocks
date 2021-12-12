c8707bc6f5d4de79f5421f3e3bf53ba4038b9821
Jorge Paravicini <j.m.paravicini@gmail.com>
2021-12-12 22:55:49 +0100

    // WIP TriangleMesh

---
+++ BlocksEngine/include/BlocksEngine/Physics/Collision/TriangleMesh.h
@@ -0,0 +1,24 @@
+﻿// 
+// Copyright (c) 2021, Severin Goddon & Jorge Paravicini
+// All rights reserved.
+// 
+// This source code is licensed under the MIT-style license found in LICENSE file in the root directory of this source tree.
+// 
+// Author: Jorge Paravicini
+// File: TriangleMesh.h
+
+#pragma once
+
+namespace BlocksEngine
+{
+    class TriangleMesh;
+}
+
+class BlocksEngine::TriangleMesh
+{
+public:
+private:
+    //------------------------------------------------------------------------------
+    // Fields
+    //------------------------------------------------------------------------------
+};
